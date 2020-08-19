4f00331cc74fdd01ada649b0e6e588033bfed5b4
YingVickyCao <ying.vicky.cao@outlook.com>
2020-08-19 15:27:34 +0800

    refactor

--- Retrofit2/src/main/java/com/example/hades/retrofit2/_5_download_zip/IDownloadProgress.java
+++
@@ -1,5 +0,0 @@
-package com.example.hades.retrofit2._5_download_zip;
-
-public interface IDownloadProgress {
-    void update(long bytesRead, long length, boolean done);
-}
--- Retrofit2android/app/src/main/java/com/example/hades/retrofit2/IDownloadProgress.java
+++
@@ -1,5 +0,0 @@
-package com.example.hades.retrofit2;
-
-public interface IDownloadProgress {
-    void update(long bytesRead, long length, boolean done);
-}
